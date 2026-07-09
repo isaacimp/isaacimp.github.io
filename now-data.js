a34b986e22d5dab104e4494f4f678d273c271ba2
Isaac <imorrisperrott@gmail.com>
2026-07-09 18:14:06 +1000

    now

--- now-data.js
+++ now-data.js
@@ -8,6 +8,13 @@ const NOW_ENTRIES = [
     date: '2026-07-09',
     html: `
       <p>Reading: <em>The Baroque Cycle</em> by Neal Stephenson, <em>The Moon Is a Harsh Mistress</em> by Robert A. Heinlein</p>
+      <p>Working: loggt Voice recorded health and life tracking</p>
+      `
+  },
+  {
+    date: '2026-07-08',
+    html: `
+      <p>Reading: <em>The Baroque Cycle</em> by Neal Stephenson, <em>The Moon Is a Harsh Mistress</em> by Robert A. Heinlein</p>
       <p>Working: Karl Popper's epistemology</p>
       <p>Building: at1a v0.3 health tracker</p>
     `
