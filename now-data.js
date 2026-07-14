86f88fcadedc9a045ecc1230831801e89b2285df
Isaac <imorrisperrott@gmail.com>
2026-07-15 00:09:43 +1000

    now page 15th

--- now-data.js
+++ now-data.js
@@ -5,12 +5,21 @@
 // homepage always shows entries[0], and /now lists all of them.
 const NOW_ENTRIES = [
   {
+    date: '2026-07-15',
+    html: `
+      <p>Reading: <em>The Baroque Cycle</em> by Neal Stephenson, <em>The Moon Is a Harsh Mistress</em> by Robert A. Heinlein</p>
+      <p>Creating a voice recording device with AI transcription for health tracking. Currently figuring out a name for it and therefore for the company too.</p>
+      <p>Currently watching <a href="https://youtu.be/YGeNzpZ5Va4?si=tnEtV_-iLaJqM6yu" target="_blank">this interview</a> with David Deutsch. I'm also trying to figure out how to think about productivity and generally getting things done without coersion.</p>
+      <p>Playing deadlock. Currently Emissary V.</p>
+      `
+  },
+  {
     date: '2026-07-09',
     html: `
       <p>Reading: <em>The Baroque Cycle</em> by Neal Stephenson, <em>The Moon Is a Harsh Mistress</em> by Robert A. Heinlein</p>
       <p>Solving: loggt Voice recorded health and life tracking</p>
       <p>I've been working on this site quite a bit recently and trying to clean up the code to make it easier to work with and publish writing and stuff here.</p>
-      <p>I'm also playing a lot of deadlock. I'm Emissary V. I am getting better but I'm in a bit of elo hell at the moment.
+      <p>I'm also playing a lot of deadlock. I'm Emissary V. I am getting better but I'm in a bit of elo hell at the moment.</p>
       `
   },
   {
