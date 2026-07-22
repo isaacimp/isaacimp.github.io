4adb01ab8e567024813e7e04807f4369d9415cad
Isaac <imorrisperrott@gmail.com>
2026-07-22 23:13:00 +1000

    now

--- now-data.js
+++ now-data.js
@@ -5,6 +5,13 @@
 // homepage always shows entries[0], and /now lists all of them.
 const NOW_ENTRIES = [
   {
+    date: '2026-07-22',
+    html: `
+      <p>Reading: <em>Seveneves</em> by Neal Stephenson, <em>Poor Charlies Almanack</em> by Peter D. Kaufman and <em>The Moon Is a Harsh Mistress</em> by Robert A. Heinlein</p>
+      <p>Building out a company: <a href="https://fourpots.com/" target="_blank">fourpots</a> where I am creating a voice recording device with transcription which coverts speech to real data for health tracking as well as potentially a air filter for cars but that might just end up as a personal project for myself.</p>
+      `
+  },
+  {
     date: '2026-07-15',
     html: `
       <p>Reading: <em>The Baroque Cycle</em> by Neal Stephenson, <em>The Moon Is a Harsh Mistress</em> by Robert A. Heinlein</p>
