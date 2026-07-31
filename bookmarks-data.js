5b47c5e801a0b912c6f12805da5b5b13fe6ff347
Isaac <imorrisperrott@gmail.com>
2026-07-31 18:08:32 +1000

    bookmark new

--- bookmarks-data.js
+++ bookmarks-data.js
@@ -12,16 +12,23 @@
 //
 const BOOKMARKS = [
   {
+     date: '2026-07-31',
+     url: 'https://takingchildrenseriously.com/wp-content/uploads/2023/12/The-Evolution-of-Culture.pdf',
+     title: 'The Evolution of Culture - David Deutsch',
+     type: 'article',
+     note: ''
+   },
+  {
     date: '2026-07-29',
     url: 'https://wiki.chadnet.org/files/using-sunlight-to-sustain-life.pdf',
-    title: 'Using Sunlight to Sustain Life',
-    type: 'paper',
-    note: 'A good explanation of light and its importance in biological energy production.'
+    title: 'Using Sunlight to Sustain Life - Ray Peat',
+    type: 'article',
+    note: 'A nice article on light and its importance.'
   },
   {
     date: '2026-07-15',
     url: 'https://youtu.be/YGeNzpZ5Va4?si=tnEtV_-iLaJqM6yu',
-    title: 'Interview with David Deutsch',
+    title: 'Cool in person interview with David Deutsch',
     type: 'video',
     note: ''
   }
