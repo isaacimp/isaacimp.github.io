2cfd34be93a6b9bce932e0dcad129ee9a7aacbd1
Isaac <imorrisperrott@gmail.com>
2026-08-06 21:21:34 +1000

    new tab no

--- now-data.js
+++ now-data.js
@@ -31,7 +31,7 @@ const NOW_ENTRIES = [
     date: '2026-07-22',
     html: `
       <p>Reading: <em>Seveneves</em> by Neal Stephenson, <em><a href="/articles/poorcharlie.html" >Poor Charlies Almanack</a></em> by Peter D. Kaufman and <em>The Moon Is a Harsh Mistress</em> by Robert A. Heinlein</p>
-      <p>Building out a company: <a href="https://fourpots.com/" target="_blank">fourpots</a> where I am creating a voice recording device with transcription which coverts speech to real data for health tracking as well as potentially an air filter for cars but that might just end up as a personal project for myself.</p>
+      <p>Building out a company: <a href="https://fourpots.com/">fourpots</a> where I am creating a voice recording device with transcription which coverts speech to real data for health tracking as well as potentially an air filter for cars but that might just end up as a personal project for myself.</p>
       `
   },
   {
@@ -39,7 +39,7 @@ const NOW_ENTRIES = [
     html: `
       <p>Reading: <em>The Baroque Cycle</em> by Neal Stephenson, <em>The Moon Is a Harsh Mistress</em> by Robert A. Heinlein</p>
       <p>Creating a voice recording device with AI transcription for health tracking. Currently figuring out a name for it and therefore for the company too.</p>
-      <p>Currently watching <a href="https://youtu.be/YGeNzpZ5Va4?si=tnEtV_-iLaJqM6yu" target="_blank">this interview</a> with David Deutsch. I'm also trying to figure out how to think about productivity and generally getting things done without coersion.</p>
+      <p>Currently watching <a href="https://youtu.be/YGeNzpZ5Va4?si=tnEtV_-iLaJqM6yu">this interview</a> with David Deutsch. I'm also trying to figure out how to think about productivity and generally getting things done without coersion.</p>
       <p>Playing deadlock. Currently Emissary V.</p>
       `
   },
