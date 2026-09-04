0e2764fbf46712213624851d21e9d326f66bbe94
Isaac <imorrisperrott@gmail.com>
2026-09-04 11:05:41 +1000

    uses imgs

--- uses-data.js
+++ uses-data.js
@@ -15,41 +15,41 @@ const USES = [
     name: 'Nothing Phone 2a',
     blurb: 'The first phone Ive had that hasnt quickly become impossible to use due to overall software slowdown or breaking and I like how it looks and works.',
     link: 'https://au.nothing.tech/collections/phones',
-    image: ''
+    image: 'img/nothing.jpg'
   },
   {
     category: 'Tech',
     name: 'Kobo Libra Colour',
     blurb: 'Really nice little device for reading digital content. Fast, light, easy to use, easy to transfer things, long battery life.',
     link: 'www.amazon.com.au/Kobo-Glare-Free-KaleidoTM-Audiobooks-Waterproof/dp/B0CZXX465Z?th=1',
-    image: ''
+    image: 'img/kobolibra.jpg'
   },
   {
     category: 'Tech',
     name: 'Insta360 Go Ultra',
     blurb: 'Very small cool camera. Have used it enough to put it on this list but will update this blurb after some more usage.',
     link: 'https://www.insta360.com/product/insta360-go-ultra',
-    image: ''
+    image: 'img/instaultra.jpg'
   },
   {
     category: 'Home',
     name: 'Ausclimate Cool Seasons Dehumidifier',
     blurb: 'This is a life saver particularly in the winter when the humidity is high. Mold grows at anywhere above 60% humidity and that is what the humidity is for basically the entire season. Any decent dehumidifier should do fine.',
     link: 'https://ausclimate.com.au/products/cool-seasons-premium-10l-desiccant-dehumidifier?srsltid=AfmBOook-TMhMcC2nQjVRArnPMF1SAgl223lSWyjH3uDc8dplCjF9Ntc',
-    image: ''
+    image: 'img/dehumy.jpg'
   },
   {
     category: 'Biology',
     name: 'NeilMed Sinus Rinse',
     blurb: 'Helps to remove allergens and dust and makes me feel clean and makes it much easier to breathe.',
     link: 'https://shopau.neilmed.com/collections/sinus-rinse',
-    image: ''
+    image: 'img/sinusrinse.jpg'
   },
   {
     category: 'Biology',
     name: 'Vivobarefoot Shoes',
     blurb: 'Barefoot shoes have been great for my feet and this pair still look brand new after 2 years.',
     link: 'https://www.vivobarefoot.com/',
-    image: ''
+    image: 'img/barefoot.jpg'
   }
 ];
